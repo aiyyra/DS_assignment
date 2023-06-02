ef0884628c7a9ab971c23cc1a48206be3b3c7525
ayyra <muhdafiq197@gmail.com>
2023-06-02 09:45:16 +0800

    changing file path for Suzume Image

--- src/main/java/page/LoginPage.java
+++ src/main/java/page/LoginPage.java
@@ -105,7 +105,7 @@ public class LoginPage extends javax.swing.JFrame {
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 210, 460, 220));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\HP\\OneDrive - Universiti Malaya\\3. DS\\Suzume\\SuzuDoor.jpg")); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/Suzu_pic/SuzuDoor.jpg"))); // NOI18N
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
